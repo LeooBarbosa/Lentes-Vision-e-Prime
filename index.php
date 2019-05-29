fa11e3fd55c5b3c5d3533b0044a7cbc3dfc6909e
LeooBarbosa <39632478+LeooBarbosa@users.noreply.github.com>
2019-05-29 16:06:42 -0300

    Update index.php

--- index.php
+++ index.php
@@ -9,63 +9,64 @@
     <title>Lentes</title>
 </head>
 <body>
-<div class="cabecalho-pag">
+    <div class="cabecalho-pag">
         <h1 class="pag-titulo">Coleta de informações</h1>
     </div>
-<div class="informar-cliente">
+
+    <div class="informar-cliente">
             <h3 class="informar-conteudo">Informamos que,</h3>
             <p>de acordo com os parâmetros passados, logo abaixo é o resultado</p>
-        </div>
-    <div id="main">
+    </div>
+
         <section class="resultado-calculo">
-        <?php
-            $grauA = isset($_GET["grauA"])?$_GET["grauA"]:null;
-            echo "<p class='resultado'>Valor 1 digitado: $grauA</p>";
-            $grauB = isset($_GET["grauB"])?$_GET["grauB"]:null;
-            echo "<p class='resultado'>Valor 2 digitado: $grauB</p>";
-            $grauC = isset($_GET["grauC"])?$_GET["grauC"]:null;
-            echo "<p class='resultado'>Valor 3 digitado: $grauC</p>";
-            $grauD = isset($_GET["grauD"])?$_GET["grauD"]:null;
-            echo "<p class='resultado'>Valor 4 digitado: $grauD</p>";
-            $tipoLente;
-            $tipoLenteDois;
-            // Lentes
-            if($grauA == null and $grauB == null){
-                $tipoLente = null;
-                echo "<p class='mensagem'>Nos falta informação para determinar a lente!</p>";
-            }
-            elseif($grauC <= 0 and $grauC >= -2 && $grauD <= 0 and $grauD >= -2){
-                if($grauA <= -3 and $grauA >= -12 && $grauB <= -3 and $grauB >= -12){
-                    $tipoLente = "Lente Prime (1) é a melhor pra você!";
-                    if($grauC < -2 and $grauC >=-5 && $grauD < -2 and $grauD){
-                        if($grauA <= -3 and $grauA >= -10 && $grauB <= -3 and $grauB >= -10){
-                            $tipoLente = "Lente Prime (2) é a melhor pra você!";
-                        }
+            <div class="resultado">
+
+                <?php
+                    $grauA = isset($_GET["grauA"])?$_GET["grauA"]:null;
+                    echo "<p class='resultado'>Valor 1 digitado: $grauA</p>";
+                    $grauB = isset($_GET["grauB"])?$_GET["grauB"]:null;
+                    echo "<p class='resultado'>Valor 2 digitado: $grauB</p>";
+                    $grauC = isset($_GET["grauC"])?$_GET["grauC"]:null;
+                    echo "<p class='resultado'>Valor 3 digitado: $grauC</p>";
+                    $grauD = isset($_GET["grauD"])?$_GET["grauD"]:null;
+                    echo "<p class='resultado'>Valor 4 digitado: $grauD</p>";
+                    $tipoLente;
+                    $tipoLenteDois;
+                    // Lente Prime
+                    if($grauA == null and $grauB == null){
+                        $tipoLente = null;
+                        echo "<p class='mensagem'>Nos falta informação para determinar a lente!</p>";
                     }
-                    echo "<p class='mensagem'>$tipoLente";
-                } else {
-                    echo "<p class='mensagem'>Valores não satisfazem nossos modelos</p>";
-                }
-            } elseif($grauA <= 0 and $grauA >= -15 && $grauB <= 0 and $grauB >= -15) {
-                    if($grauC <= -2 and $grauC >=-5 && $grauD <= -2 and $grauD >= -5){
-                    $tipoLenteDois = "Lente Vision é a ideal pra você!";
-                    echo "<p class='mensagem'>$tipoLenteDois</p>";
-                } else {
-                    echo "<p class='mensagem'>Valor de grau cilíndrico não suportado</p>";
-                }
-                
-            } else {
-                $tipoLente = null;
-                echo "<p class='mensagem'>Valores nao suportados</p>";
-            }
-                ?>
-                <a href="index.html">Voltar</a>
-    </div>
+                    elseif($grauC <= 0 and $grauC >= -2 && $grauD <= 0 and $grauD >= -2){
+                        if($grauA <= -3 and $grauA >= -12 && $grauB <= -3 and $grauB >= -12){
+                            $tipoLente = "Lente Prime (1) é a melhor pra você!";
+                            if($grauC < -2 and $grauC >=-5 && $grauD < -2 and $grauD){
+                                if($grauA <= -3 and $grauA >= -10 && $grauB <= -3 and $grauB >= -10){
+                                    $tipoLente = "Lente Prime (2) é a melhor pra você!";
+                                }
+                            }
+                            echo "<p class='mensagem'>$tipoLente";
+                        } else {
+                                    // echo "$tipoLente";
+                            echo "<p class='mensagem'>Valores não satisfazem nossos modelos</p>";
+                        }
+                    } elseif($grauA <= 0 and $grauA >= -15 && $grauB <= 0 and $grauB >= -15) {
+                            if($grauC <= -2 and $grauC >=-5 && $grauD <= -2 and $grauD >= -5){
+                            $tipoLenteDois = "Lente Vision é a ideal pra você!";
+                            echo "<p class='mensagem'>$tipoLenteDois</p>";
+                        } else {
+                            echo "<p class='mensagem'>Valor de grau cilíndrico não suportado</p>";
+                        }
+                        
+                    } else {
+                        $tipoLente = null;
+                        echo "<p class='mensagem'>Valores nao suportados</p>";
+                    }
+                        ?><br>
+                        <a href="index.html">Voltar</a>
+            </div>
         </section>
-      
-    </footer>
     </body>
-    </div>
 </html>
                 
 
